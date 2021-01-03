322deda9d6def08ceaed721960b663819ff65f5b
bqanderson <bqanderson@me.com>
2021-01-03 12:58:09 -0600

    Add site metadata via React Helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Workshop | FEM`,
+    description: 'A site built off of the Frontend Masters Introduction to Gatsby tutorial',
+    author: `bqanderson`,
   },
   plugins: ['gatsby-plugin-emotion']
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,68 +1,83 @@
 import React, { Fragment } from 'react'
 import { Global, css } from '@emotion/react'
+import Helmet from 'react-helmet'
 import Header from './Header'
+import useSiteMetadata from '../hooks/useSiteMetadata'
 
-const Layout = ({ children }) => (
-  <Fragment>
-    <Global 
-      styles={css`
-        * {
-          box-sizing: border-box;
-          margin: 0;
-        }
-
-        /* === More info: https://bit.ly/2PsCnzk === */
-        * + * {
-          margin-top: 1rem;
-        }
-
-        html,
-        body {
-          margin: 0;
-          color: #555;
-          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-              Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
-              'Segoe UI Symbol';
-          font-size: 18px;
-          line-height: 1.4;
-
-          /* === Remove margin for the div that Gatsby mounts into === */
-          > div {
-            margin-top: 0;
+const Layout = ({ children }) => {
+  const { title, description } = useSiteMetadata()
+  
+  return (
+    <Fragment>
+      <Global 
+        styles={css`
+          * {
+            box-sizing: border-box;
+            margin: 0;
           }
-        }
 
-        main {
-          margin: 2rem auto;
-          max-width: 550px;
-        }
-
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-          color: #222;
-          line-height: 1.1;
-
-          + * {
-            margin-top: 0.5rem;
+          /* === More info: https://bit.ly/2PsCnzk === */
+          * + * {
+            margin-top: 1rem;
           }
-        }
 
-        strong {
-          color: #222;
-        }
+          html,
+          body {
+            margin: 0;
+            color: #555;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+                Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
+                'Segoe UI Symbol';
+            font-size: 18px;
+            line-height: 1.4;
 
-        li {
-          margin-top: 0.25rem;
-        }
-      `}
-    />
-    <Header />
-    <main>{children}</main>
-  </Fragment>
-)
+            /* === Remove margin for the div that Gatsby mounts into === */
+            > div {
+              margin-top: 0;
+            }
+          }
+
+          main {
+            margin: 2rem auto;
+            max-width: 550px;
+          }
+
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6 {
+            color: #222;
+            line-height: 1.1;
+
+            + * {
+              margin-top: 0.5rem;
+            }
+          }
+
+          a {
+            text-decoration: none;
+          }
+
+          strong {
+            color: #222;
+          }
+
+          li {
+            margin-top: 0.25rem;
+          }
+        `}
+      />
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+      <Header />
+      <main>{children}</main>
+    </Fragment>
+  )
+}
 
 export default Layout
