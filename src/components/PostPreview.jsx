c85ccda9de451104a59632cae466b5e0746da17d
bqanderson <bqanderson@me.com>
2021-01-03 20:40:51 -0600

    Create blog posts. Display on Home page via PostPreview component

---
+++ src/components/PostPreview.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { Link } from 'gatsby'
+
+const Preview = styled('article')`
+  border-bottom: 1px solid #ddd;
+  margin-top: 0.75rem;
+  padding-bottom: 1rem;
+
+  :first-of-type {
+    margin-top: 1rem;
+  }
+`
+
+const PostPreview = ({ post }) => {
+  return (
+    <Preview>
+      <h3>
+        <Link to={post.slug}>{post.title}</Link>
+      </h3>
+      <p>{post.excerpt}</p>
+      <Link to={post.slug}>Read this Post &rarr;</Link>
+    </Preview>
+  )
+}
+
+export default PostPreview
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/Layout'
+import usePosts from '../hooks/usePosts'
+import PostPreview from '../components/PostPreview'
 
-export default () => (
-  <Layout>
-    <h1>Home</h1>
-    <p>Hello Minnesota!</p>
-    <Link to='/about/'>Learn about me &rarr;</Link>
-  </Layout>
-)
+export default () => {
+  const posts = usePosts()
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>Hello Minnesota!</p>
+      <Link to='/about/'>Learn about me &rarr;</Link>
+      <h2>Read My Blog</h2>
+      {posts.map(post => (
+        <PostPreview post={post} key={post.slug} />
+      ))}
+    </Layout>
+  )
+}
